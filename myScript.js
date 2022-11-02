90ccc7c607c99a823bee3caea2071a8d35366cff
kimkitkat <91523037+kimkitkat@users.noreply.github.com>
2022-11-02 18:26:18 +0800

    20221102

---
+++ myScript.js
@@ -0,0 +1,40 @@
+function myFunction1(){
+  document.getElementById('myTable').style.width='200px';
+}
+function myFunction2(){
+  document.getElementById('myTable').style.width='500px';
+}
+function myFunction3(){
+  document.getElementById('myTable').style.padding='1px';
+  document.getElementById('myTable').style.border='1px outset black';
+  document.getElementById('myTable').style.borderSpacing='1px';
+}
+function myFunction4(){
+  document.getElementById('myTable').style.padding='10px';
+  document.getElementById('myTable').style.borderSpacing='10px';
+  document.getElementById('myTable').style.border='10px outset black';
+}
+function myFunction5(){
+  document.getElementById('myTable').style.padding='20px';
+  document.getElementById('myTable').style.border='20px outset black';
+  document.getElementById('myTable').style.borderSpacing='20px';
+}
+function myFunction6(){
+  document.getElementById('myTable').style.backgroundColor='#d9ff66';
+}
+function myFunction7(){
+  document.getElementById('myTable').style.backgroundColor='#66d9ff';
+}
+function myFunction8(){
+  document.getElementById('myTable').style.backgroundColor='#ff66d9';
+}
+function myFunction9(){
+  document.getElementById('myTable').style.backgroundColor='#ffb366';
+}
+function myFunction10(){
+  document.getElementById('myTable').style.backgroundColor='#ffffcc';
+  document.getElementById('myTable').style.padding='1px';
+  document.getElementById('myTable').style.border='1px outset black';
+  document.getElementById('myTable').style.borderSpacing='1px';
+  document.getElementById('myTable').style.width='200px';
+}
